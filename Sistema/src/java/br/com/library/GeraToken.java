310631af139074a58c82934662ae4113d53e0af9
ti <ti@tecnologia>
2015-05-13 08:33:07 -0300

    acertado um pouco token, agora só usar a função token para gerar.

--- Sistema/src/java/br/com/library/GeraToken.java
+++ Sistema/src/java/br/com/library/GeraToken.java
@@ -12,49 +12,38 @@ Gabriel Salles
 package br.com.library;
 
 import java.security.SecureRandom;
-import java.util.Random;
 
 
 public class GeraToken {
     
     private String codigo;
-    
-    private final int tamArray = 35;
-    
     private final int tamToken = 8; //Este define o tamanho do token.
-   
-    private String[] letras = {"0","1","2","3","4","5","6","7","8","9",
-    "A","B","C","D","E","F","G","H","I","J","K","L","M",
-   "O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private final String[] letras;
+    private int i;
     
     private String TOKEN;
     
    
     public GeraToken(){  
         
-        int i;
+        this.i = 0;
+        this.letras = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};     
         this.TOKEN = "";
         this.codigo = "";
 
     }
     
-    private int tamanhoArray(){
-
-        return letras.length;
-        
-    }
-          
+     
     public String TOKEN(){
         
         String retorno = "";
         String auxiliar = "";
         
-        int i;
-        
+        int i;      
         
         SecureRandom gerado = new SecureRandom();
         
-        for(i=0;i<tamToken;i++){
+        for(i=0;i<letras.length;i++){
             
             auxiliar = pegarLetra(gerado.nextInt(23));  
             retorno += auxiliar;
@@ -65,10 +54,10 @@ public class GeraToken {
         
     }
     
-    private String pegarLetra(int numero){
+    private String pegarLetra(int $numero){
         
         String letra = "";        
-        letra = this.letras[numero]; 
+        letra = this.letras[$numero]; 
         return letra;
         
     }
